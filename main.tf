36043f5e4507181ebea0a07bd420466a0a72f1d5
Jared A Barneck <rhyous@yahoo.com>
2023-08-16 14:46:51 -0600

    Try fix

--- main.tf
+++ main.tf
@@ -29,18 +29,18 @@ module "blog_vpc" {
   }
 }
 
+
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.10.0"
 
   name = "blog"
+
   min_size            = 1
   max_size            = 2
-
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
 }
@@ -53,9 +53,9 @@ module "blog_alb" {
 
   load_balancer_type = "application"
 
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = ["${module.blog_sg.security_group_id}"]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -82,13 +82,11 @@ module "blog_alb" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog_new"
 
-  vpc_id = module.blog_vpc.public_subnets[0]
-
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
