5c1091dfa7b386c55178e853c8d36cfa39e298f0
Jared A Barneck <rhyous@yahoo.com>
2023-08-16 13:51:56 -0600

    Remove NAT gateway

--- main.tf
+++ main.tf
@@ -23,8 +23,6 @@ module "vpc" {
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-
   tags = {
     Terraform = "true"
     Environment = "dev"
