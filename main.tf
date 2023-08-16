d8c6a5754ecc5ccd1e971cd5b58df5175388aeaa
Jared A Barneck <rhyous@yahoo.com>
2023-08-16 14:42:50 -0600

    Try fix

--- main.tf
+++ main.tf
@@ -53,9 +53,9 @@ module "blog_alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = ["${module.blog_sg.security_group_id}"]
 
   target_groups = [
     {
