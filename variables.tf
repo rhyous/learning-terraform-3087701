d06a38bcaf03c393f193a30009ad925832db7a10
Jared A Barneck <rhyous@yahoo.com>
2023-08-16 15:30:21 -0600

    Fix variables

--- variables.tf
+++ variables.tf
@@ -3,40 +3,40 @@ variable "instance_type" {
   default     = "t3.nano"
 }
 
-variable "ami_filter"{
+variable "ami_filter" {
   description = "Name filter and owner for AMI"
 
-  type = object({
-    name = string
+  type    = object ({
+    name  = string
     owner = string
   })
 
   default = {
-    name = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-    owner = ["979382823631"] # Bitnami
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
   }
 }
 
 variable "environment" {
-  description = "Development environment"
-  
-  type = object({
-    name = string
+  description = "Deployment environment"
+
+  type        = object ({
+    name           = string
     network_prefix = string
   })
 
   default = {
-    name = "dev"
+    name           = "dev"
     network_prefix = "10.0"
   }
 }
 
 variable "asg_min" {
-  description = "Minimum number of instances in the ASG"
-  default = 1
+  description = "Minimum instance count for the ASG"
+  default     = 1
 }
 
 variable "asg_max" {
-  description = "Maximum number of instances in the ASG"
-  default = 2
+  description = "Maximum instance count for the ASG"
+  default     = 2
 }
